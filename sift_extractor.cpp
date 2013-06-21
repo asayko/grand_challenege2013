719138ef9c757e72d43f25a390e55c13922c14bb
asayko <asayko@asayko-osx.local>
2013-06-21 16:05:49 -0400

    Adding self-made base64 decoder.

--- sift_extractor.cpp
+++ sift_extractor.cpp
@@ -10,14 +10,8 @@
 #include <opencv2/nonfree/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-
-#include <jpeglib.h>
-
 #include <boost/tokenizer.hpp>
 #include <boost/algorithm/string.hpp>
-#include <boost/archive/iterators/binary_from_base64.hpp>
-#include <boost/archive/iterators/transform_width.hpp>
-#include <boost/cstdint.hpp>
 
 #include <algorithm>
 #include <vector>
@@ -27,8 +21,60 @@
 #include <cstdlib>
 #include <cassert>
 
+static const char base64_bkw[] =
+"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+"\0\0\0\0\0\0\0\0\0\0\0\76\0\76\0\77\64\65\66\67\70\71\72\73\74\75\0\0\0\0\0\0"
+"\0\0\1\2\3\4\5\6\7\10\11\12\13\14\15\16\17\20\21\22\23\24\25\26\27\30\31\0\0\0\0\77"
+"\0\32\33\34\35\36\37\40\41\42\43\44\45\46\47\50\51\52\53\54\55\56\57\60\61\62\63\0\0\0\0\0"
+"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+
+inline void uudecode_1(char *dst, unsigned char *src) {
+    dst[0] = char((base64_bkw[src[0]] << 2) | (base64_bkw[src[1]] >> 4));
+    dst[1] = char((base64_bkw[src[1]] << 4) | (base64_bkw[src[2]] >> 2));
+    dst[2] = char((base64_bkw[src[2]] << 6) | base64_bkw[src[3]]);
+}
+
+size_t Base64Decode(void* dst, const char* b, const char* e) {
+    size_t n = 0;
+
+    if ((e - b) % 4) {
+        throw std::logic_error("incorrect input length for base64 decode");
+    }
+
+    while (b < e) {
+        uudecode_1((char*)dst + n, (unsigned char*)b);
+
+        b += 4;
+        n += 3;
+    }
+
+    if (n > 0) {
+        if (b[-1] == ','  || b[-1] == '=') {
+            n--;
+
+            if (b[-2] == ',' || b[-2] == '=') {
+                n--;
+            }
+        }
+    }
+    return n;
+}
+
 void GetBinaryFromBase64(const std::string & str64, std::vector<char> & binData) {
+	binData.resize(str64.size());
+	size_t t = Base64Decode(&*binData.begin(), str64.c_str(), str64.c_str() + str64.size());
+	binData.resize(t);
+
+	/*
 	binData.clear();
+	boost::archive::iterators::detail::to_6_bit<char> t;
+	for (size_t i = 0; i < str64.size(); ++i) {
+		std::cerr << t(str64[i]) == -1 << "\n";
+	}
+	std::cerr << std::endl;
 
 	typedef boost::archive::iterators::transform_width<
 	    boost::archive::iterators::binary_from_base64<char *>, 8, 6> TToBinnaryTransformerIter;
@@ -37,6 +83,7 @@ void GetBinaryFromBase64(const std::string & str64, std::vector<char> & binData)
 			TToBinnaryTransformerIter(&*str64.begin()),
 			TToBinnaryTransformerIter(&*str64.end()),
 			std::back_inserter(binData));
+	*/
 }
 
 int main() {
