611b558819cb9c27ce1e5b2878e11da6a11b2411
ton4eg <ton4a@list.ru>
2013-06-28 19:47:44 +0400

    single -> int32 vocabs

--- makevocab/calcHists.m
+++ makevocab/calcHists.m
@@ -14,16 +14,19 @@ load('vocabs/vocab_l216384.mat');
 kdtree = vl_kdtreebuild(vocab) ;
 
 %%
+z =  1:16384;
+v = int32(vocab);
 parfor i=1:numel(files)
     i
     histFile = [tmp files(i).name '_hist.mat'];
     if ~exist(histFile, 'file')
         try
             featureFile = [outpath files(i).name '.mat'];
-            [f,d] = loadd(featureFile)
-            dd = single(d);
-            [idxs, distance] = vl_kdtreequery(kdtree, vocab, dd);
-            hist = histc(idxs, 1:16384);
+            [f,d] = loadd(featureFile);
+            %dd = single(d);
+            %[idxs, distance] = vl_kdtreequery(kdtree, vocab, dd);
+            idxs =  vl_ikmeanspush(d,v);
+            hist = histc(idxs, z);
     
             fakeSave(histFile, hist);
         catch exc
