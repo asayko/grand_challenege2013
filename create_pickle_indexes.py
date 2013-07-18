f7609a0d833d14e852972bcfc8dbd248ac420656
asayko <asayko@asayko-osx.local>
2013-07-18 23:35:45 +0400

    vis_words to pickle

--- create_pickle_indexes.py
+++ create_pickle_indexes.py
@@ -12,22 +12,19 @@ import pickle
 wnl = nltk.WordNetLemmatizer()
 
 click_log_file = "/Users/asayko/data/grand_challenge/Train/TrainClickLog10K.tsv"
+visual_words_file = "./vis_words_10000.tsv"
 
 query_index_save_to_file = "./query_index.pkl"
 unigramm_index_save_to_file = "./unigramm_index.pkl"
 bigramm_index_save_to_file = "./bigramm_index.pkl"
 trigramm_index_save_to_file = "./trigramm_index.pkl"
+visual_words_save_to_file = "./vis_words_index.pkl"
 
 min_clicks_needed = 1
 
 dont_load_unigramms_for_img_ids_file = "./dont_load_unigramms_for_pics.txt"
-dont_load_unigramms_for_img_ids = set([t.strip() for t in codecs.open(dont_load_unigramms_for_img_ids_file, "r", "utf-8")])
-
 dont_load_bigramms_for_img_ids_file = "./dont_load_bigramms_for_pics.txt"
-dont_load_bigramms_for_img_ids = set([t.strip() for t in codecs.open(dont_load_bigramms_for_img_ids_file, "r", "utf-8")])
-
 dont_load_trigramms_for_img_ids_file = "./dont_load_trigramms_for_pics.txt"
-dont_load_trigramms_for_img_ids = set([t.strip() for t in codecs.open(dont_load_trigramms_for_img_ids_file, "r", "utf-8")])
 
 stop_words = set(stopwords.words('english'))
 
@@ -40,6 +37,7 @@ unigramm_stop_words = set(['2000', '2001', '2002', '2003', '2004', '2005', '2006
                             '2010', '2011', '2012', '2013', '2014', 'dr', 'pdf', 'jpeg', 'jpg', 'com', 'www'])
 
 def GetLemmas(query):
+    query.replace(".", " ")
     query_tokens = nltk.word_tokenize(query)
     query_lemmas = []
     for token in query_tokens:
@@ -108,7 +106,37 @@ def ParseClickLogAndCreateNGrammsIndexes(click_log_file):
         
     return query_index, unigramm_index, bigramm_index, trigramm_index
 
+def ParseVisualWordsBase(visual_words_file):
+    fin = codecs.open(visual_words_file, "r", "utf-8")
+    num_lines_processed = 0
+    
+    print >> sys.stderr,  "Reading visual words from %s" % visual_words_file
+    
+    vis_words_index = {}
+    
+    for line in fin.xreadlines():
+        if num_lines_processed % 5000 == 0: print >> sys.stderr,  "%d lines processed" % num_lines_processed
+        line_parts = line.split("\t")
+        img_id = line_parts[0].strip()
+        vis_words_str = line_parts[1].strip()
+        vis_words = collections.Counter([int(w) for w in vis_words_str.split()])
+        vis_words_index[img_id] = vis_words
+        num_lines_processed = num_lines_processed + 1
+
+    return vis_words_index
+
+
 if __name__ == "__main__":
+    
+    dont_load_unigramms_for_img_ids = set([t.strip() for t in codecs.open(dont_load_unigramms_for_img_ids_file, "r", "utf-8")])
+    dont_load_bigramms_for_img_ids = set([t.strip() for t in codecs.open(dont_load_bigramms_for_img_ids_file, "r", "utf-8")])
+    dont_load_trigramms_for_img_ids = set([t.strip() for t in codecs.open(dont_load_trigramms_for_img_ids_file, "r", "utf-8")])
+    
+    visual_words_index = ParseVisualWordsBase(visual_words_file)
+    
+    print >> sys.stderr, "Saving %s" % visual_words_save_to_file
+    pickle.dump(visual_words_index, open(visual_words_save_to_file, "w"))
+    
     query_index, unigramm_index, bigramm_index, trigramm_index = ParseClickLogAndCreateNGrammsIndexes(click_log_file)
 
     print >> sys.stderr, "Saving %s" % query_index_save_to_file
