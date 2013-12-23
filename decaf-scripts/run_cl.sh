02559db10303dc74c6244e815cf3cd479eb66d4a
ton4eg <slesarev.anton@gmail.com>
2013-12-23 14:41:37 +0400

    sh

---
+++ decaf-scripts/run_cl.sh
@@ -0,0 +1,11 @@
+python cl.py //home/asayko/data/grand_challenge/Train/images_jpeg_renamed/ /home/slesarev/train_decaf/ 0 log0.txt &> 0.txt &
+python cl.py //home/asayko/data/grand_challenge/Train/images_jpeg_renamed/ /home/slesarev/train_decaf/ 1 log1.txt &> 1.txt &
+python cl.py //home/asayko/data/grand_challenge/Train/images_jpeg_renamed/ /home/slesarev/train_decaf/ 2 log2.txt &> 2.txt &
+python cl.py //home/asayko/data/grand_challenge/Train/images_jpeg_renamed/ /home/slesarev/train_decaf/ 3 log3.txt &> 3.txt &
+python cl.py //home/asayko/data/grand_challenge/Train/images_jpeg_renamed/ /home/slesarev/train_decaf/ 4 log4.txt &> 4.txt &
+python cl.py //home/asayko/data/grand_challenge/Train/images_jpeg_renamed/ /home/slesarev/train_decaf/ 5 log5.txt &> 5.txt &
+python cl.py //home/asayko/data/grand_challenge/Train/images_jpeg_renamed/ /home/slesarev/train_decaf/ 6 log6.txt &> 6.txt &
+python cl.py //home/asayko/data/grand_challenge/Train/images_jpeg_renamed/ /home/slesarev/train_decaf/ 7 log7.txt &> 7.txt &
+python cl.py //home/asayko/data/grand_challenge/Train/images_jpeg_renamed/ /home/slesarev/train_decaf/ 8 log8.txt &> 8.txt &
+python cl.py //home/asayko/data/grand_challenge/Train/images_jpeg_renamed/ /home/slesarev/train_decaf/ 9 log9.txt &> 9.txt &
+
